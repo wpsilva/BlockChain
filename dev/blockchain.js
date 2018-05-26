9596207e62ae7866539136694233bd1d79e0ab77
Wagner Pereira da Silva <wagnerpdasilva@hotmail.com>
2018-05-26 15:20:05 -0300

    Add hash method and sha256 package

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,3 +1,5 @@
+const sha256 = require('sha256')
+
 function Blockchain(){
     this.chain = [];
     this.pendingTransactions = [];
@@ -33,6 +35,12 @@ Blockchain.prototype.createNewTransaction = function (amount, sender, recipient)
     this.pendingTransactions.push(newTransaction);
 
     return this.getLastBlock()['index'] + 1;
+}
+
+Blockchain.prototype.hashBlock = function (previousBlockHash, currentBlockData, nonce){
+    const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+    const hash = sha256(dataAsString);
+    return hash;
 }
 
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -3,15 +3,37 @@ const Blockchain = require('./blockchain');
 
 const wagnerCoin = new Blockchain();
 
-wagnerCoin.createNewBlock(0, 'hash01', 'hash02');
-wagnerCoin.createNewTransaction(100, 'wagnerUHA786786HJ', 'MARIIHIU6786');
+const previousBlockHash = 'hash01';
+const currentBlockData = [
+    {
+        amount:10
+        , sender: 'wagnerUHA786786HJ'
+        , recipient:'MARIIHIU6786A'
+    },
+    {
+        amount:20
+        , sender: 'wagnerUHA786786HJ'
+        , recipient:'MARIIHIU6786B'
+    },
+    {
+        amount:30
+        , sender: 'wagnerUHA786786HJ'
+        , recipient:'MARIIHIU6786C'
+    }
+];
 
-wagnerCoin.createNewBlock(1, 'hash02', 'hash03');
-wagnerCoin.createNewTransaction(50, 'wagnerUHA786786HJ', 'MARIIHIU6786');
-wagnerCoin.createNewTransaction(100, 'wagnerUHA786786HJ', 'MARIIHIU6786');
-wagnerCoin.createNewTransaction(200, 'wagnerUHA786786HJ', 'MARIIHIU6786');
+const nonce = 100;
 
-wagnerCoin.createNewBlock(3, 'hash03', 'hash04');
+console.log(wagnerCoin.hashBlock(previousBlockHash, currentBlockData, nonce));
 
-console.log(wagnerCoin);
-console.log(wagnerCoin.chain[2]);
+//wagnerCoin.createNewBlock(0, 'hash01', 'hash02');
+//wagnerCoin.createNewTransaction(100, 'wagnerUHA786786HJ', 'MARIIHIU6786');
+
+//wagnerCoin.createNewBlock(1, 'hash02', 'hash03');
+//wagnerCoin.createNewTransaction(50, 'wagnerUHA786786HJ', 'MARIIHIU6786');
+//wagnerCoin.createNewTransaction(100, 'wagnerUHA786786HJ', 'MARIIHIU6786');
+//wagnerCoin.createNewTransaction(200, 'wagnerUHA786786HJ', 'MARIIHIU6786');
+//wagnerCoin.createNewBlock(3, 'hash03', 'hash04');
+
+//console.log(wagnerCoin);
+//console.log(wagnerCoin.chain[2]);
