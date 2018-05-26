7e73c27aa2d1306a49d87afda65eaae2728b71aa
Wagner Pereira da Silva <wagnerpdasilva@hotmail.com>
2018-05-26 12:17:20 -0300

    createNewBlock and createNewTransaction methods

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,22 +1,38 @@
 function Blockchain(){
     this.chain = [];
-    this.newTransactions = [];
+    this.pendingTransactions = [];
 }
 
 Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash){
-    const newBlock = new {
+    const newBlock = {
         index:this.chain.length+1
         , timestamp : Date.now()
-        , transactions : this.newTransactions
+        , transactions : this.pendingTransactions
         , nonce: nonce
         , previousBlockHash: previousBlockHash
         , hash: hash
     }
 
-    this.newTransactions = [];
+    this.pendingTransactions = [];
     this.chain.push(newBlock);
 
     return newBlock;
+}
+
+Blockchain.prototype.getLastBlock = function (){
+    return this.chain[this.chain.length -1];
+}
+
+Blockchain.prototype.createNewTransaction = function (amount, sender, recipient){
+    const newTransaction = {
+        amount : amount
+        , sender : sender
+        , recipient : recipient
+    };
+
+    this.pendingTransactions.push(newTransaction);
+
+    return this.getLastBlock()['index'] + 1;
 }
 
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -3,4 +3,15 @@ const Blockchain = require('./blockchain');
 
 const wagnerCoin = new Blockchain();
 
+wagnerCoin.createNewBlock(0, 'hash01', 'hash02');
+wagnerCoin.createNewTransaction(100, 'wagnerUHA786786HJ', 'MARIIHIU6786');
+
+wagnerCoin.createNewBlock(1, 'hash02', 'hash03');
+wagnerCoin.createNewTransaction(50, 'wagnerUHA786786HJ', 'MARIIHIU6786');
+wagnerCoin.createNewTransaction(100, 'wagnerUHA786786HJ', 'MARIIHIU6786');
+wagnerCoin.createNewTransaction(200, 'wagnerUHA786786HJ', 'MARIIHIU6786');
+
+wagnerCoin.createNewBlock(3, 'hash03', 'hash04');
+
 console.log(wagnerCoin);
+console.log(wagnerCoin.chain[2]);
